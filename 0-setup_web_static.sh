b619cead1884b0d09a563f051f57358259d9b58c
kedir m <kedir10mohamed45@gmail.com>
2023-01-24 06:34:20 +0000

    added two files

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,21 +4,32 @@
 apt-get update
 apt-get install -y nginx
 
-mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-
-echo \
-"<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
-
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
 
-sed -z 's/\tserver_name _;/\tserver_name _;\n\tlocation \/hbnb_static \{\n\t\talias \/data\/web_static\/current\/;\n\t\tindex index.html index.htm;\n\t\}/' -i /etc/nginx/sites-available/default
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
 service nginx restart
